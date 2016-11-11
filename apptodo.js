b0fe7ef99961853c5115d060de9431bdf0c6053a
weerapat julagate <weerapat julagate>
2016-11-12 00:22:58 +0700

    change name controller

--- apptodo.js
+++ apptodo.js
@@ -2,8 +2,8 @@ angular.module('TDApp', [])
   .service('TodoService', function () {
     var text = this
     text.titles = [
-    {title : 'Kantapon',done : true},
-    {title : 'Surapong',done : false}
+      {title : 'Kantapon Buddha',done : true},
+      {title : 'Weerapat Chulaket',done : false}
     ]
     text.list = function () {
         return text.titles
@@ -13,27 +13,30 @@ angular.module('TDApp', [])
         return text.titles.push(word)
       }
     }
-    text.ch = function(ch) {
-      if(ch == true)
-        return 'Complete'
+    text.ch = function() {
+      if(text.titles.done == true)
+        text.titles.done = false
       else
-        return ''
+        text.titles.done = true
     }
   })
-  .controller('ListData', function ($scope, TodoService) {
+  .controller('ShowListController', function ($scope, TodoService) {
      $scope.titles = TodoService.list()
   })
-  .controller('CheckBox', function ($scope,TodoService) {
-    $scope.checktf = function(ch){
-      if(ch.done == true)
-        ch.done = false;
-      else
-        ch.done = true 
-      }
-    })
-  .controller('AddmyTexts', function($scope, TodoService) {
+  .controller('CheckBoxController', function ($scope, TodoService) {
+      $scope.checktf = function(ch) {
+        if(ch.done == true)
+          ch.done = false;
+        else
+          ch.done = true
+       }
+  })
+  .controller('AddTextsController', function($scope, TodoService) {
     $scope.save = function() {
-      var word = { title: $scope.title ,done : false}
+      var word = {
+        title: $scope.title ,
+        done : false
+      }
       TodoService.add(word)
       $scope.title = null
     }
